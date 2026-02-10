c38ee4a8e1ea15e80a6fc2e755b5ca1a989e7199
glennmichael123 <gtorregosa@gmail.com>
2026-02-10 18:55:24 +0800

    chore: wip

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -9,4 +9,4 @@ export type {
 export { greet, formatGreeting } from "./utils/greeting.js";
 export { createLogger } from "./utils/logger.js";
 
-export const VERSION = "0.3.0";
+export const VERSION = "0.2.9";
